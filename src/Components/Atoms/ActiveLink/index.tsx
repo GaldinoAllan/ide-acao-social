a869ab81daaf2605dddf6ba7380a1c8cebbb849d
joaovsoliveira <jvsolivert@DESKTOP-523M2F3>
2024-08-21 21:33:26 -0300

    solves conflict

--- src/Components/Atoms/ActiveLink/index.tsx
+++ src/Components/Atoms/ActiveLink/index.tsx
@@ -2,7 +2,6 @@
 
 import { ReactElement, cloneElement } from "react";
 import Link, { LinkProps } from "next/link";
-import { useEffect, useState } from 'react';
 
 interface ActiveLinkProps extends LinkProps {
   children: ReactElement
@@ -10,12 +9,7 @@ interface ActiveLinkProps extends LinkProps {
 }
 
 export function ActiveLink({ children, activeClassName, ...rest }: ActiveLinkProps) {
-  const [asPath, setAsPath] = useState<string>('');
-
-  useEffect(() => {
-    setAsPath(`/${window.location.hash}`);
-  }, []);
-
+  const asPath = `/${window.location.hash}`
   const className = asPath === rest.href
     ? activeClassName
     : ''
